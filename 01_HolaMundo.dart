9d69d2bda3d0d59119fc79bea6303a561bf8b6fd
Naysix25 <nayelisixto04@gmail.com>
2026-04-21 13:55:18 -0600

    Add files via upload

---
+++ 01_HolaMundo.dart
@@ -0,0 +1,10 @@
+void main() {
+  //var myName = 'Nayeli';
+  //String myName = 'Nayeli';
+  late final myName;
+  
+  myName = 'Nayeli Sixto';
+  
+  print ('Hola, ${myName.toUpperCase()}');
+  
+}
